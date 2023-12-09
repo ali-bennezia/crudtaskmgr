4ea2fbf718fefea61a0d4f12f0b3350791374266
ali-bennezia <bennezia.ali@gmail.com>
2023-12-09 22:58:41 +0100

    project initialization

--- backend/models/taskModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-	author: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "user" },
-	title: { type: String, required: true, minlength: 3, maxlength: 28 },
-	content: { type: String, required: true, minlength: 3, maxlength: 60 }
-});
-
-module.exports = mongoose.model( "task", schema );
--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const controller = require("./../controllers/userController.js");
-const router = express.Router();
-
-router.post( "/register", controller.register );
-router.post( "/signin", controller.signIn );
-
-module.exports = router;
